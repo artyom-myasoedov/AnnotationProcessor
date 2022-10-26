5374b7b8474c9cea65888bdb8cd279f36f826ce7
ruaaaxs <Artem.Myasoedov@raiffeisen.ru>
2022-10-26 23:24:42 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/cs/processor/ParalleledAnnotationProcessor.java
@@ -0,0 +1,19 @@
+package ru.vsu.cs.processor;
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@SupportedAnnotationTypes({"ru.vsu.cs.annotation.Paralleled"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Processor.class)
+public class ParalleledAnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
